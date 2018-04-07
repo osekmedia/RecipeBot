e04e2164e33bf31552242ac53f3c30d1c39c5220
osekmedia <osekmedia@gmail.com>
2018-04-07 00:05:39 -0700

    Adding transactional annotation to resolve hibernate random lazy load error

--- src/main/java/com/osekmedia/recipeBot/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/osekmedia/recipeBot/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.osekmedia.recipeBot.repositories.CategoryRepository;
 import com.osekmedia.recipeBot.repositories.RecipeRepository;
@@ -34,6 +35,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
     
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
     	
         recipeRepository.saveAll( getRecipes() );
