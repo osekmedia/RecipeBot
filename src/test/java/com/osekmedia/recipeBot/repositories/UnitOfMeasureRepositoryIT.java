716ce46b8218da930f9c03af18eb9e7448dcc0b0
osekmedia <osekmedia@gmail.com>
2018-04-19 00:17:34 -0700

    adding UOM integration test

---
+++ src/test/java/com/osekmedia/recipeBot/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,39 @@
+package com.osekmedia.recipeBot.repositories;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.osekmedia.recipeBot.models.UnitOfMeasure;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+	
+	@Autowired
+	UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindByDescriptionTeaspoon() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+	
+	@Test
+	public void testFindByDescriptionCup() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+		assertEquals("Cup", uomOptional.get().getDescription());
+	}
+
+}
